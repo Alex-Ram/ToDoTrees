1e815dfba241afbbf637dfb058304239af7af753
Nubzoar <nubzoar@users.noreply.github.com>
2018-01-10 20:12:32 -0800

    Added classes to represent the tables in the database.

---
+++ ToDoTrees/Category.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoTrees
+{
+    class Category
+    {
+        public int? CategoryID { get; set; }
+        public int? ParentID { get; set; }
+        public string Name { get; set; }
+
+        public Category(Category c)
+            : this(c.CategoryID, c.ParentID, c.Name) {}
+
+        public Category(int? ID, int? parentID, string name)
+        {
+            CategoryID = ID;
+            ParentID = parentID;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ ToDoTrees/ToDoItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoTrees
+{
+    class ToDoItem
+    {
+        public int? ToDoItemID { get; set; }
+        public int CategoryID { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public ToDoItem(ToDoItem i)
+            : this(i.ToDoItemID, i.CategoryID, i.Description, i.IsCompleted) {}
+
+        public ToDoItem(int? id, int categoryID, string desc)
+            : this(id, categoryID, desc, false) {}
+
+        public ToDoItem(int? id, int categoryID, string desc, bool isCompleted)
+        {
+            ToDoItemID = id;
+            CategoryID = categoryID;
+            Description = desc;
+            IsCompleted = isCompleted;
+        }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}
