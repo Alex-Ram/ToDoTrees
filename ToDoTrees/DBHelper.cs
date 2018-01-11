e4064244d8e4473c742fcb7bd56c61ffa4dd1924
Nubzoar <nubzoar@users.noreply.github.com>
2018-01-10 23:29:15 -0800

    Added static database helper classes for Categories and ToDoItems with basic CRUD functionality.

---
+++ ToDoTrees/DBHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoTrees
+{
+    static class DBHelper
+    {
+        /// <summary>
+        /// Returns the connection string required to connect to the database.
+        /// </summary>
+        /// <returns>The connection string.</returns>
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=ToDoTreesDB;Integrated Security=True");
+        }
+    }
+}
